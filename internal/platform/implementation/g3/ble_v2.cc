d5791629707bf707fbb68910d730781fdf36e6fb
edwinwu <edwinwu@google.com>
2022-03-18 09:00:19 -0700

    [BLE Refactor] Handle advertisement header in ProcessFoundAdvertisement for DiscoveredPeripheralTracker.
    PiperOrigin-RevId: 435652048

--- internal/platform/implementation/g3/ble_v2.cc
+++ internal/platform/implementation/g3/ble_v2.cc
@@ -77,20 +77,8 @@ bool BleV2Medium::StartAdvertising(
 
   absl::MutexLock lock(&mutex_);
   auto& env = MediumEnvironment::Instance();
-  // If `advertising_data.service_uuids` is empty, then it is fast
-  // advertisement. In real case, this should be the CopresenceServiceUuid or
-  // the fastAdvertisementUuid.
-  bool is_fast_advertisement = !advertising_data.service_uuids.empty();
-  for (const auto& service_data : scan_response_data.service_data) {
-    // Interested item found in the first index.
-    advertisement_byte_ = service_data.second;
-    if (!advertisement_byte_.Empty()) {
-      break;
-    }
-  }
-  if (advertisement_byte_.Empty()) return false;
-  env.UpdateBleV2MediumForAdvertising(is_fast_advertisement, true, *this,
-                                      &advertisement_byte_);
+  env.UpdateBleV2MediumForAdvertising(
+      /*enabled=*/true, *this, adapter_->GetPeripheralV2(), scan_response_data);
   return true;
 }
 
@@ -99,20 +87,29 @@ bool BleV2Medium::StopAdvertising() {
   absl::MutexLock lock(&mutex_);
   advertisement_byte_ = {};
 
-  auto& env = MediumEnvironment::Instance();
-  env.UpdateBleV2MediumForAdvertising(
-      /*is_fast_advertisement=*/false,
-      /*enabled=*/false, *this, &advertisement_byte_);
+  BleAdvertisementData empty_advertisement_data = {};
+  MediumEnvironment::Instance().UpdateBleV2MediumForAdvertising(
+      /*enabled=*/false, *this, adapter_->GetPeripheralV2(),
+      empty_advertisement_data);
   return true;
 }
 
 bool BleV2Medium::StartScanning(const std::vector<std::string>& service_uuids,
-                                PowerMode power_mode,
-                                ScanCallback scan_callback) {
-  return false;
+                                PowerMode power_mode, ScanCallback callback) {
+  NEARBY_LOGS(INFO) << "G3 Ble StartScanning";
+
+  MediumEnvironment::Instance().UpdateBleV2MediumForScanning(
+      true, std::move(callback), *this);
+  return true;
 }
 
-bool BleV2Medium::StopScanning() { return false; }
+bool BleV2Medium::StopScanning() {
+  NEARBY_LOGS(INFO) << "G3 Ble StopScanning";
+
+  auto& env = MediumEnvironment::Instance();
+  env.UpdateBleV2MediumForScanning(false, {}, *this);
+  return true;
+}
 
 std::unique_ptr<api::ble_v2::GattServer> BleV2Medium::StartGattServer(
     ServerGattConnectionCallback callback) {
